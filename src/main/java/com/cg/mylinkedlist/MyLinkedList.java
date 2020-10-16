3f3994d764efbda6237466632ba26b327c617d74
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-16 19:37:00 +0530

    Created a new linked list

--- src/main/java/com/cg/mylinkedlist/MyLinkedList.java
+++ src/main/java/com/cg/mylinkedlist/MyLinkedList.java
@@ -2,5 +2,15 @@ package com.cg.mylinkedlist;
 
 public class MyLinkedList {
     Node head=null;
+
+    public void print()
+    {
+        Node n=head;
+        while(n!=null)
+        {
+            System.out.print(n.data+" -> ");
+            n=n.next;
+        }
+    }
 }
 
